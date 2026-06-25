cdee09a70b2cb6d9a57b67961605d40f7bd4d98e
leonelizzetti <leonelizzetti1@gmail.com>
2026-06-25 08:36:39 -0500

    feat: guardando modulo del mapa

--- src/app/components/mapa/mapa.ts
+++ src/app/components/mapa/mapa.ts
@@ -19,20 +19,32 @@ export class MapaComponent implements AfterViewInit {
       attribution: '&copy; OpenStreetMap contributors'
     }).addTo(this.map);
 
+    const customIcon = L.icon({
+      iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+      shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
+      shadowSize: [41, 41]
+    });
+
+    const markerGroup = L.featureGroup();
+
     this.incidenteService.listarIncidentes().subscribe({
       next: (data: any[]) => {
         if (Array.isArray(data)) {
           data.forEach(item => {
             if (item.latitud && item.longitud) {
-              L.circleMarker([item.latitud, item.longitud], {
-                color: '#e74c3c',
-                fillColor: '#c0392b',
-                fillOpacity: 0.8,
-                radius: 10
-              }).addTo(this.map)
+              const marker = L.marker([item.latitud, item.longitud], { icon: customIcon })
+                .addTo(this.map)
                 .bindPopup(`<b>Incidente:</b> ${item.descripcion}`);
+              
+              markerGroup.addLayer(marker);
             }
           });
+          if (markerGroup.getLayers().length > 0) {
+            this.map.fitBounds(markerGroup.getBounds().pad(0.2));
+          }
         }
       },
       error: (err) => console.error("Error:", err)
